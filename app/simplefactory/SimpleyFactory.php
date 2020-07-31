5497de7068e8f49e551b81b6b7114935b4d1d6d0
gwx <704835519@qq.com>
2020-07-31 17:02:34 +0800

    a

--- app/simplefactory/SimpleyFactory.php
+++ app/simplefactory/SimpleyFactory.php
@@ -19,3 +19,70 @@ class SimpleFactory
         return $player;
     }
 }
+
+class MusicPlayerFactory
+{
+    public static function create ($type)
+    {
+        if ($type == 'QQ') {
+            $player = new QQPlayer();
+        } else if ($type == 'Wy') {
+            $player = new WyPlayer();
+        } else if ($type == 'KG') {
+            $player = new KGPlayer();
+        } else {
+            $player = null;
+        }
+
+        return $player;
+    }
+}
+
+interface IMusicPlayerFactory
+{
+    static function create ();
+}
+
+class QQPlayerFactory implements IMusicPlayerFactory
+{
+    public static function create ()
+    {
+        return new QQPlayer();
+    }
+}
+
+class WyPlayerFactory implements IMusicPlayerFactory
+{
+    public static function create ()
+    {
+        return new WyPlayer();
+    }
+}
+
+class KGPlayerFactory implements IMusicPlayerFactory
+{
+    public static function create ()
+    {
+        return new KGPlayer();
+    }
+}
+
+class MusicPlayerFactoryMap
+{
+    const Players = [
+        'QQ' => 'QQPlayerFactory',
+        'Wy' => 'WyPlayerFactory',
+        'KG' => 'KGPlayerFactory'
+    ];
+
+    public static function getPlayerFactory (string $type)
+    {
+        if (empty($type)) {
+            return null;
+        }
+
+        return (self::Players[$type])::create();
+    }
+}
+
+MusicPlayerFactoryMap::getPlayerFactory('QQ');
